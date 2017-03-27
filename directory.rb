6f6cc396c5cb528c85936770ec54136433a18457
Paul Kassar <your email>
2017-03-27 15:31:10 +0100

    6th

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
-students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"alex DeLarge",
-"The Wicked Witch of the WEst",
-"The Terminator",
-"Freddy Krueger",
-"The joker",
-"Joffrey Baratheon",
-"Norman Bates",
+students =[
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the WEst", cohort: :november},
+{name: "The Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november},
 ]
 
 
@@ -18,14 +18,14 @@ def print_header
   puts "---------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
 
