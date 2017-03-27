07863c361e4890cc4fa7165c2ba53e93e55f74ce
Paul Kassar <your email>
2017-03-27 15:40:40 +0100

    two versions now

---
+++ directory2.rb
@@ -0,0 +1,34 @@
+def input_students
+  puts "please enter the names of the students"
+  puts "to finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  return students
+end
+
+def print_header
+  puts "The students of Villians Academy"
+  puts "---------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
